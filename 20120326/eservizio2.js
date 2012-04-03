a23015c3b3902ff2f5ca315ba9110da4542f7a52
Luca Menichetti <meniluca@gmail.com>
2012-04-03 22:44:30 +0200

    first commit

--- 20120326/eservizio2.js
+++ 20120326/eservizio2.js
@@ -1,11 +1,31 @@
-var domain1 = DOMAIN([[1.5,5.5]])([4])
-DRAW(domain1);
-HIDE(domain1);
+//DOMAIN (intervalli)(dominio)
+var domain = DOMAIN([[1,2]])([10]);
 
-var domain2 = DOMAIN([[1.5,5.5],[1,3]])([4,2]) 
-DRAW(domain2);
-HIDE(domain2);
+//la funzione mapping accetta un punto
+// e viene invocata su tutti i punti del dominio
+//prende il punto e lo alza di uno su un asse
+var mapping = function (p) {
+	var u = p[0];
 
-var domain3 = DOMAIN([[1.5,5.5],[1,3],[0,1]])([4,2,1])
-DRAW(domain3);
-HIDE(domain3);
+	return [u, 1];
+}
+
+var mapped = MAP(mapping)(domain);
+
+DRAW(mapped);
+
+///////////////////////////////////////
+//eserzizio sbagliato!!!
+var domain2 = DOMAIN([[1,2]])([5]);
+
+var mappingCube = function (p) {
+
+	var u = p[0];
+
+	return [u,u,u];
+
+};
+
+var mapped = MAP(mappingCube)(domain2);
+
+DRAW(mapped);
