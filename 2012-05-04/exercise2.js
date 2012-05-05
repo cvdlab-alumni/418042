24e67b28336cd4ef08a7b0d7c0ef4ad681a2755f
Luca Menichetti <meniluca@gmail.com>
2012-05-05 12:55:51 +0200

    esercizio 2

--- 2012-05-04/exercise2.js
+++ 2012-05-04/exercise2.js
@@ -3,8 +3,10 @@
 	Produce the model of the fuselage (local coordinate system).
 */
 
-var p0 = [[0,0,-1.8],[2,0,-1.8],[3,0,0],[2,0,3],[0,0,3],[-2,0,2],
-	[-2,0,0],[-2,0,-1.6],[0,0,-1.8]];
+//var p0 = [[0,0,-1.8],[1.8,0,-1.8],[1.8,0,1.8],[-1.8,0,1.8],[-1.8,0,-1.8],[0,0,-1.8]];
+
+var p0 = [[0,0,-1.3],[1.3,0,-1.3],[1.8,0,0],[2,0,2.4],[0,0,2.4],[-2,0,2.4],
+	[-1.8,0,0],[-1.3,0,-1.3],[0,0,-1.3]];
 
 var c0 = BEZIER(S0)(p0);
 var domain1 = INTERVALS(1)(100);
@@ -13,17 +15,14 @@ DRAW(circle);
 
 var p1 = p0.map(function (p) {return [p[0],p[1]-0.5,p[2]]});
 var c1 = BEZIER(S0)(p1);
-var scaledCircle = MAP (c1)(domain1);
 
 var p2 = p0.map(function (p) {return [p[0],p[1]-0.8,p[2]]});
 var c2 = BEZIER(S0)(p2);
-var scaledCircle = MAP (c2)(domain1);
 
 var p3 = p0.map(function (p) {return [p[0]*0.5,p[1]-1,p[2]*0.5]});
 var c3 = BEZIER(S0)(p3);
-var scaledCircle = MAP (c3)(domain1);
 
-var domain2 = DOMAIN([[0,1],[0,1]])([30,50]);
+var domain2 = DOMAIN([[0,1],[0,1]])([100,50]);
 
 var cockPitMapping = BEZIER(S1)([c0,c1,c2,c3]);
 var cockPit = MAP(cockPitMapping)(domain2);
@@ -32,11 +31,68 @@ DRAW(cockPit);
 
 //back cockpit
 
-var p4 = p0.map(function (p) {return [p[0],p[1]+1,p[2]]});
+var p4 = p0.map(function (p) {return [p[0],p[1]+0.1,p[2]]});
 var c4 = BEZIER(S0)(p4);
-var scaledCircle = MAP(c4)(domain1);
 
-var p5 = p0.map(function (p) {return [p[0],p[1]+1,p[2]]});
+var p5 = p0.map(function (p) {return [p[0]*0.95,p[1]+1.3,p[2]*0.95]});
 var c5 = BEZIER(S0)(p5);
-var scaledCircle = MAP(c5)(domain1);
 
+var backCockPitMapping = BEZIER(S1)([c0,c4,c5]);
+var backCockPit = MAP(backCockPitMapping)(domain2);
+DRAW(backCockPit); 
+
+//middle
+
+var domain3 = DOMAIN([[0,1],[0,1]])([30,1]);
+
+var p6 = p5.map(function (p) {return [p[0]*0.95,p[1]+1,p[2]*0.95]});
+var c6 = BEZIER(S0)(p6);
+
+var middleCockPitMapping = BEZIER(S1)([c5,c6]);
+var middleCockPit = MAP(middleCockPitMapping)(domain3);
+DRAW(middleCockPit); 
+
+// body
+
+var domain4 = DOMAIN([[0,1],[0,1]])([30,1]);
+
+p7 = p6.map(function (p) {return [p[0],p[1],p[2]]});
+var c7 = BEZIER(S0)(p7);
+
+var tailControlPoints = [[0,0,-1],
+	[1,0,-1],[1,0,-1],[1,0,-1],[1,0,-1],[1,0,-1],[1,0,-1],[1,0,-1],[1,0,-1],
+	[1,0,0],
+	[1,0,1],[1,0,1],[1,0,1],[1,0,1],[1,0,1],[1,0,1],
+	[0,0,1],
+	[-1,0,1],[-1,0,1],[-1,0,1],[-1,0,1],[-1,0,1],[-1,0,1],
+	[-1,0,0],
+	[-1,0,-1],[-1,0,-1],[-1,0,-1],[-1,0,-1],[-1,0,-1],[-1,0,-1],[-1,0,-1],
+	[0,0,-1]];
+
+var p8 = tailControlPoints.map(function (p) {return [p[0]*0.6,p[1]+6.3,p[2]*0.6+0.4]});
+var c8 = BEZIER(S0)(p8);
+
+var bodyMapping = BEZIER(S1)([c7,c8]);
+var body = MAP(bodyMapping)(domain4);
+DRAW(body);
+
+
+//tail
+
+var p9 = [[0,0,-1],[0.5,0,0],[0,0,1],[-0.5,0,0],[0,0,-1],];
+
+function POLYPOINT(points) {
+	return SIMPLICIAL_COMPLEX(points)(
+		points.map( function (p,i) {
+			return [i];
+		}
+		));
+}
+
+p9 = p9.map(function (p) {return [p[0]*0.4,p[1]+11,p[2]*0.4+0.6]});
+var c9 = BEZIER(S0)(p9);
+var tailMapping = BEZIER(S1)([c8,c9]);
+var tail = MAP(tailMapping)(domain4);
+DRAW(tail);
+
+DRAW(POLYPOINT(p9))
